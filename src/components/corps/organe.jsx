24a8ac2fbdeafc8244e3cfb59da45ec73c53ff4b
Hei <alix.lejeune.4+github@gmail.com>
2025-10-07 13:33:55 +0200

    corps et organe update + base css

--- src/components/corps/organe.jsx
+++ src/components/corps/organe.jsx
@@ -1,11 +1,10 @@
 export default function Organe({nom, image, taille, code}){
     return(
         <div>
-            <Button onClick={()=>alert(`code: ${code}`)}>
-                <img src={image} alt="" />
-                width={taille}
-                height={taille}
-            </Button>
+            <button onClick={()=>alert(`code: ${code}`)}>
+                <img src={image} alt={nom} width={taille} height={taille}/>
+                
+            </button>
         </div>
     )
 }
