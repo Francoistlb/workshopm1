e82fe921befd9a488283a33772e280b7b72f1288
AlixLejeune <alix.lejeune.4+github@gmail.com>
2025-10-08 18:17:04 +0200

    update images for body, updated package.json to host vite on self ip (the daemon server is not hosting yet)

--- src/components/corps/corps.jsx
+++ src/components/corps/corps.jsx
@@ -8,20 +8,20 @@ function answerCheck(){
 export default function Corps(){
     return(
         <div id="body">   
-            <Organe nom="Cerveau" image="https://upload.wikimedia.org/wikipedia/commons/3/37/Human-brain-vector.svg" taille={100} code="1" />
+            <Organe nom="Cerveau" image="src/assets/organs/brain.svg" taille={100} code="1" />
             <div id="lungs">
-                <Organe nom="Poumon" image="https://upload.wikimedia.org/wikipedia/commons/0/04/Birikak.png" taille={100} code="2" />
-                <Organe nom="Poumon" image="https://upload.wikimedia.org/wikipedia/commons/0/04/Birikak.png" taille={100} code="2" />
+                <Organe nom="Poumon" image="src/assets/organs/l_lung.png" taille={100} code="2" />
+                <Organe nom="Cœur" image="src/assets/organs/heart.svg" taille={100} code="3" />
+                <Organe nom="Poumon" image="src/assets/organs/r_lung.png" taille={100} code="2" />
             </div>
-            <Organe nom="Cœur" image="https://upload.wikimedia.org/wikipedia/commons/c/c8/Diagram_of_the_human_heart_%28clean%29.svg" taille={100} code="3" />
             <div id="liverandstomch" >
-                <Organe nom="Foie" image="https://upload.wikimedia.org/wikipedia/commons/d/d4/Liver.svg" taille={100} code="4" />
-                <Organe nom="Estomac" image="https://upload.wikimedia.org/wikipedia/commons/b/be/Stomach_icon.svg" taille={100} code="5" />    
+                <Organe nom="Foie" image="src/assets/organs/liver.svg" taille={100} code="4" />
+                <Organe nom="Estomac" image="src/assets/organs/stomach.svg" taille={100} code="5" />    
             </div>
-            <Organe nom="Intestin" image="https://upload.wikimedia.org/wikipedia/commons/b/b5/Intestine_-_sized.png" taille={100} code="6" />
+            <Organe nom="Intestin" image="src/assets/organs/guts.png" taille={100} code="6" />
             <div id="kidneys">
-                <Organe nom="Rein" image="https://upload.wikimedia.org/wikipedia/commons/8/80/202501_Normal_Kidney.svg" taille={70} code="7" />
-                <Organe nom="Rein" image="https://upload.wikimedia.org/wikipedia/commons/8/80/202501_Normal_Kidney.svg" taille={70} code="7" />
+                <Organe nom="Rein" image="src/assets/organs/l_kidney.png" taille={70} code="7" />
+                <Organe nom="Rein" image="src/assets/organs/r_kidney.png" taille={70} code="7" />
         </div>
         <div id="answer">
             <form action="" method="get">
